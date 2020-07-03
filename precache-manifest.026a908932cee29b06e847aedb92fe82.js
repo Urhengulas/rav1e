d1cde1a3aeed75f06b767f0e773958d7bb882416
Urhengulas <Urhengulas@users.noreply.github.com>
2020-07-03 12:36:04 +0000

    deploy: 6f30d86738ba81c789a51210abed2d1505867adc

---
+++ precache-manifest.026a908932cee29b06e847aedb92fe82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e52fe172bd1cd03753",
+    "url": "/522f129150afa29e975b.module.wasm"
+  },
+  {
+    "revision": "c54f68c9ab770ee919609d3d73c9725a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e52fe172bd1cd03753",
+    "url": "/static/js/2.09782ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09782ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac782c197c0325e7d37",
+    "url": "/static/js/3.4079e1fc.chunk.js"
+  },
+  {
+    "revision": "f6b83bd5d3abd809ba98",
+    "url": "/static/js/main.a2f32fce.chunk.js"
+  },
+  {
+    "revision": "97f23729c92978c239ec",
+    "url": "/static/js/runtime-main.ee1ec117.js"
+  }
+]);
